186bdc049bbe59d682d7085980e65e1ca7939c0a
OveZ <41060185+ovezthaking@users.noreply.github.com>
2024-06-09 22:43:33 +0200

    Update client.c
    client messages

--- client.c
+++ client.c
@@ -5,197 +5,139 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <errno.h>
 
-#define MAX_CONNECTION 100
-#define NAME_SIZE 8
-#define MAX_DATA 100000
-#define DATA_PORT_BASE 7000
 #define BUFFER_SIZE 1000
+#define DATA_SIZE 400000
+#define MAX_DATA 100000
 
-struct CALCDATA {
-    uint32_t data[MAX_DATA];
-};
-
-struct conninfo {
-    int status;
-    char name[NAME_SIZE + 1];
-    int Csock;
-    int protocol_state;
-    int Asock;
-    int Dsock;
-    int dataport;
-    struct CALCDATA data;
-};
-
-int lsock;
-struct conninfo conn[MAX_CONNECTION];
-uint32_t histogram[16];
+uint32_t bitcnt[16];
 
 void clear_histogram() {
-    memset(histogram, 0, sizeof(histogram));
+    memset(bitcnt, 0, sizeof(bitcnt));
 }
 
 void load_histogram(uint32_t value) {
     uint32_t mask = 1;
     for (int i = 0; i < 16; i++) {
-        if (value & mask) histogram[i]++;
+        if (value & mask) bitcnt[i]++;
         mask = mask * 2;
     }
 }
 
-int create_data(int idx, struct CALCDATA *cdata) {
-    if (cdata != NULL) {
-        uint32_t i;
-        uint32_t *value;
-        uint32_t v;
-        value = &cdata->data[0];
-        for (i = 0; i < MAX_DATA; i++) {
-            v = (uint32_t)rand() ^ (uint32_t)rand();
-            printf("Creating value #%d v=%u addr=%lu \r", i, v, (unsigned long)value);
-            *value = v & 0x0000FFFF;
-            value++;
-        }
-        return 1;
+void calculate_histogram(uint32_t *data, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        load_histogram(data[i]);
     }
-    return 0;
 }
 
-void send_protocol(int idx, char cmd, const char *param) {
+void send_protocol(int sock, const char *name, char cmd, const char *param) {
     char sdata[BUFFER_SIZE];
-    snprintf(sdata, sizeof(sdata), "@000000000!%c:%s#", cmd, param);
-    send(conn[idx].Csock, sdata, strlen(sdata), MSG_NOSIGNAL | MSG_DONTWAIT);
-}
-
-void handle_client_command(int idx, char *command) {
-    printf("Received command from client: %s\n", command);
-    if (command[0] == 'N') {
-        conn[idx].dataport = DATA_PORT_BASE + idx + 1;
-        int create_result = create_data(idx, &conn[idx].data);
-        if (create_result != 0) {
-            char sdata[BUFFER_SIZE];
-            sprintf(sdata, "%d", conn[idx].dataport);
-            send_protocol(idx, 'P', sdata);
-            conn[idx].protocol_state = 2;
-        } else {
-            send_protocol(idx, 'X', "0");
-        }
-    } else if (command[0] == 'R') {
-        // process result (to be implemented)
-    } else if (command[0] == 'E') {
-        // handle error (to be implemented)
-    }
+    snprintf(sdata, sizeof(sdata), "@%s!%c:%s#", name, cmd, param);
+    send(sock, sdata, strlen(sdata), MSG_NOSIGNAL | MSG_DONTWAIT);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <name> <server_addr> <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int port = atoi(argv[1]);
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    fd_set read_fds;
-    int max_sd, activity, new_socket;
+    char *name = argv[1];
+    char *server_addr = argv[2];
+    int port = atoi(argv[3]);
+
+    int sock;
+    struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE];
 
-    // Initialize listening socket
-    lsock = socket(AF_INET, SOCK_STREAM, 0);
-    if (lsock == 0) {
-        perror("Socket failed");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
-    memset(&server_addr, '0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
 
-    if (bind(lsock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(lsock);
+    if (inet_pton(AF_INET, server_addr, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address or address not supported");
+        close(sock);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(lsock, 10) < 0) {
-        perror("Listen failed");
-        close(lsock);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        close(sock);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d\n", port);
+    printf("Connected to server %s:%d\n", server_addr, port);
 
-    // Initialize connections
-    for (int i = 0; i < MAX_CONNECTION; i++) {
-        conn[i].status = 0;
-        conn[i].Csock = -1;
-        conn[i].Asock = -1;
-        conn[i].Dsock = -1;
-        conn[i].protocol_state = 0;
-        conn[i].dataport = 0;
-    }
+    send_protocol(sock, name, 'N', "0");
 
     while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(lsock, &read_fds);
-        max_sd = lsock;
-
-        for (int i = 0; i < MAX_CONNECTION; i++) {
-            if (conn[i].Csock > 0) {
-                FD_SET(conn[i].Csock, &read_fds);
-            }
-            if (conn[i].Csock > max_sd) {
-                max_sd = conn[i].Csock;
-            }
+        int valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            perror("Read error");
+            close(sock);
+            exit(EXIT_FAILURE);
         }
+        buffer[valread] = '\0';
+        printf("Received from server: %s\n", buffer);
 
-        activity = select(max_sd + 1, &read_fds, NULL, NULL, NULL);
+        if (buffer[11] == 'P') {
+            int dataport;
+            sscanf(buffer + 13, "%d", &dataport);
 
-        if ((activity < 0) && (errno != EINTR)) {
-            perror("Select error");
-        }
-
-        if (FD_ISSET(lsock, &read_fds)) {
-            if ((new_socket = accept(lsock, (struct sockaddr *)&client_addr, &client_len)) < 0) {
-                perror("Accept error");
+            int data_sock;
+            struct sockaddr_in data_addr;
+            if ((data_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                perror("Data socket creation error");
                 exit(EXIT_FAILURE);
             }
 
-            int idx;
-            for (idx = 0; idx < MAX_CONNECTION; idx++) {
-                if (conn[idx].Csock == -1) {
-                    conn[idx].Csock = new_socket;
-                    conn[idx].status = 1;
-                    printf("New connection accepted, assigned index %d\n", idx);
-                    break;
-                }
+            data_addr.sin_family = AF_INET;
+            data_addr.sin_port = htons(dataport);
+            if (inet_pton(AF_INET, server_addr, &data_addr.sin_addr) <= 0) {
+                perror("Invalid data address or address not supported");
+                close(data_sock);
+                exit(EXIT_FAILURE);
             }
-            if (idx == MAX_CONNECTION) {
-                fprintf(stderr, "Max connections reached\n");
-                close(new_socket);
-            }
-        }
 
-        for (int i = 0; i < MAX_CONNECTION; i++) {
-            if (FD_ISSET(conn[i].Csock, &read_fds)) {
-                int valread = read(conn[i].Csock, buffer, BUFFER_SIZE);
-                if (valread == 0) {
-                    printf("Client disconnected, index %d\n", i);
-                    close(conn[i].Csock);
-                    conn[i].Csock = -1;
-                    conn[i].status = 0;
-                } else {
-                    buffer[valread] = '\0';
-                    handle_client_command(i, buffer + 11); // Assuming command starts at index 11
-                }
+            if (connect(data_sock, (struct sockaddr *)&data_addr, sizeof(data_addr)) < 0) {
+                perror("Data connection failed");
+                close(data_sock);
+                exit(EXIT_FAILURE);
             }
+
+            uint32_t data[MAX_DATA];
+            size_t received = 0;
+            while (received < DATA_SIZE) {
+                int bytes = read(data_sock, (char *)data + received, DATA_SIZE - received);
+                if (bytes <= 0) {
+                    perror("Data read error");
+                    close(data_sock);
+                    exit(EXIT_FAILURE);
+                }
+                received += bytes;
+            }
+            close(data_sock);
+
+            clear_histogram();
+            calculate_histogram(data, MAX_DATA);
+
+            char result[BUFFER_SIZE];
+            snprintf(result, sizeof(result), "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u", bitcnt[0], bitcnt[1], bitcnt[2], bitcnt[3], bitcnt[4], bitcnt[5], bitcnt[6], bitcnt[7], bitcnt[8], bitcnt[9], bitcnt[10], bitcnt[11], bitcnt[12], bitcnt[13], bitcnt[14], bitcnt[15]);
+            send_protocol(sock, name, 'R', result);
+        } else if (buffer[11] == 'X') {
+            printf("No new data. Exiting...\n");
+            break;
+        } else if (buffer[11] == 'D') {
+            printf("Result accepted. Exiting...\n");
+            break;
         }
     }
 
-    close(lsock);
+    close(sock);
     return 0;
 }
